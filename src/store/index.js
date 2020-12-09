6a2687e060cbc5aaf11152ce991685552b4d84d2
JTend <joseg.tenderor@gmail.com>
2020-12-09 02:38:05 -0400

    Add token in the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: { token : 'b2c6de510d584484d74c9aa9f8fa9f04' },
-  getters : { getKey : state => state.token },
-  mutations: { setPet(state, id) { state.pet = id; } },
+  state: { token : '' },
+  
+  getters : {  },
+  
+  mutations: {  },
+
   actions: {  },
+
   modules: {  }
 })
